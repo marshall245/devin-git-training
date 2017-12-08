21d1c8e3f29e9f727ea443669a21b3723600d068
mmarkham@precisionlender.com <mmarkham@precisionlender.com>
2017-12-08 18:11:02 -0500

    initial commit. adding README and a little function file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Devin's introduction to Git and GitHub
+
+Contains Python code that we wrote while demonstrating the basics.
---
+++ little_functions.py
@@ -0,0 +1,12 @@
+# a library of little functions
+
+
+__author__ = "Marshall Markham"
+
+
+def traingles(n):
+    """Returns the triangle number n * ( n + 1 ) / 2"""
+    return n * (n + 1) / 2
+    
+def handshake(n):
+    return n * (n - 1) / 2
