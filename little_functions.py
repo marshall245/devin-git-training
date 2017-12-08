2a2a5edf26b455862ba8f6ff7df40da7b061e662
admin <admin@homes-mbp.attlocal.net>
2017-12-08 18:31:34 -0500

    Add a circle perimeter function

--- little_functions.py
+++ little_functions.py
@@ -12,3 +12,6 @@ def handshake(n):
     """Returns the handshake number n * (n - 1) / 2"""
     return n * (n - 1) / 2
 
+def circle_perimeter(radius):
+    """Returns the perimeter of a circle """
+    return 3.14 * radius ** 2
