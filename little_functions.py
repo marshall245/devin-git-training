3cb83b9f93e35903765c8932fdeb27d1d63c2d65
mmarkham@precisionlender.com <mmarkham@precisionlender.com>
2017-12-08 18:50:57 -0500

    added spaces where needed

--- little_functions.py
+++ little_functions.py
@@ -7,10 +7,12 @@ __author__ = "Marshall Markham"
 def traingles(n):
     """Returns the triangle number n * ( n + 1 ) / 2"""
     return n * (n + 1) / 2
-    
+
+
 def handshake(n):
     """Returns the handshake number n * (n - 1) / 2"""
     return n * (n - 1) / 2
+
 
 def circle_perimeter(radius):
     """Returns the perimeter of a circle """
